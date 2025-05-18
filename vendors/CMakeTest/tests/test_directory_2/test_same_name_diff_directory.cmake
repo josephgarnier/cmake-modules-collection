62b9e94ef813196e8a172297413e18ca7fc91a0d
Joseph Garnier <garnjose@gmail.com>
2025-05-18 09:44:07 +0200

    Replace dependency by submodule

--- vendors/CMakeTest/tests/test_directory_2/test_same_name_diff_directory.cmake
+++
@@ -1,8 +0,0 @@
-#[[[
-# This file exists to test ct_add_dir(),
-there exists another test with the same name in the main tests/ directory.
-#]]
-ct_add_test(NAME test_same_name_diff_dir)
-function(${test_same_name_diff_dir})
-
-endfunction()
