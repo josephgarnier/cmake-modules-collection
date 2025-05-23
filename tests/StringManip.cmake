939224125f354db40c78ccd9e7834b643ae7c9fe
Joseph Garnier <garnjose@gmail.com>
2025-05-23 23:37:02 +0200

    Initial commit

---
+++ tests/StringManip.cmake
@@ -0,0 +1,36 @@
+# Copyright 2025-present, Joseph Garnier
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# =============================================================================
+# What Is This?
+# -------------
+# See README file in the root directory of this source tree.
+
+#-------------------------------------------------------------------------------
+# Test the StringManip module|Public command
+ct_add_test(NAME "test_string_manip")
+function(${CMAKETEST_TEST})
+	include(FuncStringManip)
+
+	ct_add_section(NAME "public_command")
+	function(${CMAKETEST_SECTION})
+	
+		ct_add_section(NAME "throws_if_unknown_argument" EXPECTFAIL)
+		function(${CMAKETEST_SECTION})
+			string_manip(FOO)
+			ct_assert_prints("Unrecognized arguments: \"FOO\"")
+		endfunction()
+		
+		ct_add_section(NAME "throws_if_arg_op_is_missing_1" EXPECTFAIL)
+		function(${CMAKETEST_SECTION})
+			string_manip()
+		endfunction()
+		
+		ct_add_section(NAME "throws_if_arg_op_is_missing_2" EXPECTFAIL)
+		function(${CMAKETEST_SECTION})
+			string_manip(BAR)
+		endfunction()
+	endfunction()
+endfunction()
---
+++ tests/StringManipSplit.cmake
@@ -0,0 +1,69 @@
+# Copyright 2025-present, Joseph Garnier
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# =============================================================================
+# What Is This?
+# -------------
+# See README file in the root directory of this source tree.
+
+#-------------------------------------------------------------------------------
+# Test the StringManip module|SPLIT operation: ``string_manip(SPLIT <string> <output_list_var>)``
+ct_add_test(NAME "test_string_manip_split_operation")
+function(${CMAKETEST_TEST})
+	include(FuncStringManip)
+
+	# Functionalities checking
+	ct_add_section(NAME "no_split_point_detected")
+	function(${CMAKETEST_SECTION})
+		string_manip(SPLIT "mystringtosplit" output)
+		ct_assert_string(output)
+		ct_assert_equal(output "mystringtosplit")
+		string_manip(SPLIT "my1string2to3split" output)
+		ct_assert_string(output)
+		ct_assert_equal(output "my1string2to3split")
+	endfunction()
+	
+	ct_add_section(NAME "split_on_uppercase")
+	function(${CMAKETEST_SECTION})
+		string_manip(SPLIT "myStringToSplit" output)
+		ct_assert_list(output)
+		ct_assert_equal(output "my;String;To;Split")
+	endfunction()
+
+	ct_add_section(NAME "split_on_non_alphanumeric")
+	function(${CMAKETEST_SECTION})
+		string_manip(SPLIT "my-string/to*split" output)
+		ct_assert_list(output)
+		ct_assert_equal(output "my;string;to;split")
+	endfunction()
+
+	ct_add_section(NAME "split_on_multiple_criteria")
+	function(${CMAKETEST_SECTION})
+		string_manip(SPLIT "myString_to*Split" output)
+		ct_assert_list(output)
+		ct_assert_equal(output "my;String;to;Split")
+	endfunction()
+
+	# Errors checking
+	ct_add_section(NAME "throws_if_arg_string_is_missing_1" EXPECTFAIL)
+	function(${CMAKETEST_SECTION})
+		string_manip(SPLIT)
+	endfunction()
+
+	ct_add_section(NAME "throws_if_arg_string_is_missing_2" EXPECTFAIL)
+	function(${CMAKETEST_SECTION})
+		string_manip(SPLIT "")
+	endfunction()
+
+	ct_add_section(NAME "throws_if_arg_output_var_is_missing_1" EXPECTFAIL)
+	function(${CMAKETEST_SECTION})
+		string_manip(SPLIT "foo")
+	endfunction()
+
+	ct_add_section(NAME "throws_if_arg_output_var_is_missing_2" EXPECTFAIL)
+	function(${CMAKETEST_SECTION})
+		string_manip(SPLIT foo)
+	endfunction()
+endfunction()
