ac25cc411603a5180fec76e9cde3bcf4bfed59b3
Joseph Garnier <garnjose@gmail.com>
2025-05-25 11:09:34 +0200

    Initial commit

---
+++ tests/StringManipExtractInterface.cmake
@@ -0,0 +1,142 @@
+# Copyright 2025-present, Joseph Garnier
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# =============================================================================
+# What Is This?
+# -------------
+# See README file in the root directory of this source tree.
+
+#-------------------------------------------------------------------------------
+# Test of [StringManip module::EXTRACT_INTERFACE operation]:
+#    ``string_manip(EXTRACT_INTERFACE <string_var> <BUILD|INSTALL> [OUTPUT_VARIABLE <output_var>])``
+ct_add_test(NAME "test_string_manip_extract_interface_operation")
+function(${CMAKETEST_TEST})
+	include(FuncStringManip)
+
+	# Functionalities checking
+	ct_add_section(NAME "extract_build_interface")
+	function(${CMAKETEST_SECTION})
+		set(input "before;$<BUILD_INTERFACE:src/file-a.cpp;src/file-b.cpp;src/file-c.cpp>;between;$<INSTALL_INTERFACE:include/file-a.h;include/file-b.h;include/file-c.h>;after")
+		string_manip(EXTRACT_INTERFACE input BUILD)
+		ct_assert_list(input)
+		ct_assert_equal(input "src/file-a.cpp;src/file-b.cpp;src/file-c.cpp")
+	endfunction()
+
+	ct_add_section(NAME "extract_build_interface_output_version")
+	function(${CMAKETEST_SECTION})
+		set(input "before;$<BUILD_INTERFACE:src/file-a.cpp;src/file-b.cpp;src/file-c.cpp>;between;$<INSTALL_INTERFACE:include/file-a.h;include/file-b.h;include/file-c.h>;after")
+		unset(output)
+		string_manip(EXTRACT_INTERFACE input BUILD OUTPUT_VARIABLE output)
+		ct_assert_equal(input "before;$<BUILD_INTERFACE:src/file-a.cpp;src/file-b.cpp;src/file-c.cpp>;between;$<INSTALL_INTERFACE:include/file-a.h;include/file-b.h;include/file-c.h>;after")
+		ct_assert_list(output)
+		ct_assert_equal(output "src/file-a.cpp;src/file-b.cpp;src/file-c.cpp")
+	endfunction()
+
+	ct_add_section(NAME "extract_install_interface")
+	function(${CMAKETEST_SECTION})
+		set(input "before;$<BUILD_INTERFACE:src/file-a.cpp;src/file-b.cpp;src/file-c.cpp>;between;$<INSTALL_INTERFACE:include/file-a.h;include/file-b.h;include/file-c.h>;after")
+		string_manip(EXTRACT_INTERFACE input INSTALL)
+		ct_assert_list(input)
+		ct_assert_equal(input "include/file-a.h;include/file-b.h;include/file-c.h")
+	endfunction()
+	
+	ct_add_section(NAME "extract_install_interface_output_version")
+	function(${CMAKETEST_SECTION})
+		set(input "before;$<BUILD_INTERFACE:src/file-a.cpp;src/file-b.cpp;src/file-c.cpp>;between;$<INSTALL_INTERFACE:include/file-a.h;include/file-b.h;include/file-c.h>;after")
+		unset(output)
+		string_manip(EXTRACT_INTERFACE input INSTALL OUTPUT_VARIABLE output)
+		ct_assert_equal(input "before;$<BUILD_INTERFACE:src/file-a.cpp;src/file-b.cpp;src/file-c.cpp>;between;$<INSTALL_INTERFACE:include/file-a.h;include/file-b.h;include/file-c.h>;after")
+		ct_assert_list(output)
+	endfunction()
+
+	ct_add_section(NAME "extract_nothing_build_interface")
+	function(${CMAKETEST_SECTION})
+		set(input "before;$<BUILD_INTERFACE:>;between;$<INSTALL_INTERFACE:include/file-a.h;include/file-b.h;include/file-c.h>;after")
+		string_manip(EXTRACT_INTERFACE input BUILD)
+		ct_assert_string(input)
+		ct_assert_equal(input "")
+	endfunction()
+
+	ct_add_section(NAME "extract_nothing_build_interface_output_version")
+	function(${CMAKETEST_SECTION})
+		set(input "before;$<BUILD_INTERFACE:>;between;$<INSTALL_INTERFACE:include/file-a.h;include/file-b.h;include/file-c.h>;after")
+		unset(output)
+		string_manip(EXTRACT_INTERFACE input BUILD OUTPUT_VARIABLE output)
+		ct_assert_equal(input "before;$<BUILD_INTERFACE:>;between;$<INSTALL_INTERFACE:include/file-a.h;include/file-b.h;include/file-c.h>;after")
+		ct_assert_string(output)
+		ct_assert_equal(output "")
+	endfunction()
+
+	ct_add_section(NAME "extract_nothing_install_interface")
+	function(${CMAKETEST_SECTION})
+		set(input "before;$<BUILD_INTERFACE:src/file-a.cpp;src/file-b.cpp;src/file-c.cpp>;between;$<INSTALL_INTERFACE:>;after")
+		string_manip(EXTRACT_INTERFACE input INSTALL)
+		ct_assert_string(input)
+		ct_assert_equal(input "")
+	endfunction()
+
+	ct_add_section(NAME "extract_nothing_install_interface_output_version")
+	function(${CMAKETEST_SECTION})
+		set(input "before;$<BUILD_INTERFACE:src/file-a.cpp;src/file-b.cpp;src/file-c.cpp>;between;$<INSTALL_INTERFACE:>;after")
+		unset(output)
+		string_manip(EXTRACT_INTERFACE input INSTALL OUTPUT_VARIABLE output)
+		ct_assert_equal(input "before;$<BUILD_INTERFACE:src/file-a.cpp;src/file-b.cpp;src/file-c.cpp>;between;$<INSTALL_INTERFACE:>;after")
+		ct_assert_string(output)
+		ct_assert_equal(output "")
+	endfunction()
+
+	# Errors checking
+	ct_add_section(NAME "throws_if_arg_string_var_is_missing_1" EXPECTFAIL)
+	function(${CMAKETEST_SECTION})
+		string_manip(EXTRACT_INTERFACE)
+	endfunction()
+
+	ct_add_section(NAME "throws_if_arg_string_var_is_missing_2" EXPECTFAIL)
+	function(${CMAKETEST_SECTION})
+		string_manip(EXTRACT_INTERFACE "")
+	endfunction()
+
+	ct_add_section(NAME "throws_if_arg_string_var_is_missing_3" EXPECTFAIL)
+	function(${CMAKETEST_SECTION})
+		string_manip(EXTRACT_INTERFACE "foo")
+	endfunction()
+
+	ct_add_section(NAME "throws_if_arg_string_var_is_missing_4" EXPECTFAIL)
+	function(${CMAKETEST_SECTION})
+		unset(input)
+		string_manip(EXTRACT_INTERFACE input)
+	endfunction()
+
+	ct_add_section(NAME "throws_if_arg_interface_type_is_missing" EXPECTFAIL)
+	function(${CMAKETEST_SECTION})
+		set(input "mystring")
+		string_manip(EXTRACT_INTERFACE input)
+	endfunction()
+
+	ct_add_section(NAME "throws_if_arg_output_var_is_missing_1" EXPECTFAIL)
+	function(${CMAKETEST_SECTION})
+		set(input "mystring")
+		string_manip(EXTRACT_INTERFACE input BUILD OUTPUT_VARIABLE)
+	endfunction()
+
+	ct_add_section(NAME "throws_if_arg_output_var_is_missing_2" EXPECTFAIL)
+	function(${CMAKETEST_SECTION})
+		set(input "mystring")
+		string_manip(EXTRACT_INTERFACE input BUILD OUTPUT_VARIABLE "")
+	endfunction()
+
+	ct_add_section(NAME "throws_if_arg_output_var_is_missing_3" EXPECTFAIL)
+	function(${CMAKETEST_SECTION})
+		set(input "mystring")
+		string_manip(EXTRACT_INTERFACE input BUILD OUTPUT_VARIABLE "foo")
+	endfunction()
+
+	ct_add_section(NAME "throws_if_arg_output_var_is_missing_4" EXPECTFAIL)
+	function(${CMAKETEST_SECTION})
+		set(input "mystring")
+		unset(output)
+		string_manip(EXTRACT_INTERFACE input BUILD OUTPUT_VARIABLE output)
+	endfunction()
+endfunction()
---
+++ tests/StringManipStripInterfaces.cmake
@@ -0,0 +1,155 @@
+# Copyright 2025-present, Joseph Garnier
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# =============================================================================
+# What Is This?
+# -------------
+# See README file in the root directory of this source tree.
+
+#-------------------------------------------------------------------------------
+# Test of [StringManip module::STRIP_INTERFACES operation]:
+#    ``string_manip(`STRIP_INTERFACES`_ <string_var> [OUTPUT_VARIABLE <output_var>])``
+ct_add_test(NAME "test_string_manip_strip_interfaces_operation")
+function(${CMAKETEST_TEST})
+	include(FuncStringManip)
+
+	# Functionalities checking
+	ct_add_section(NAME "strip_interfaces")
+	function(${CMAKETEST_SECTION})
+		set(input "libA;$<BUILD_INTERFACE:src>;libB;libC;$<INSTALL_INTERFACE:include/>libD")
+		string_manip(STRIP_INTERFACES input)
+		ct_assert_list(input)
+		ct_assert_equal(input "libA;libB;libClibD")
+
+		set(input "libA$<BUILD_INTERFACE:src>libB$<INSTALL_INTERFACE:include/>")
+		string_manip(STRIP_INTERFACES input)
+		ct_assert_string(input)
+		ct_assert_equal(input "libAlibB")
+	endfunction()
+
+	ct_add_section(NAME "strip_interfaces_output_version")
+	function(${CMAKETEST_SECTION})
+		set(input "libA;$<BUILD_INTERFACE:src>;libB;libC;$<INSTALL_INTERFACE:include/>libD")
+		unset(output)
+		string_manip(STRIP_INTERFACES input OUTPUT_VARIABLE output)
+		ct_assert_equal(input "libA;$<BUILD_INTERFACE:src>;libB;libC;$<INSTALL_INTERFACE:include/>libD")
+		ct_assert_list(output)
+		ct_assert_equal(output "libA;libB;libClibD")
+
+		set(input "libA$<BUILD_INTERFACE:src>libB$<INSTALL_INTERFACE:include/>")
+		unset(output)
+		string_manip(STRIP_INTERFACES input OUTPUT_VARIABLE output)
+		ct_assert_equal(input "libA$<BUILD_INTERFACE:src>libB$<INSTALL_INTERFACE:include/>")
+		ct_assert_string(output)
+		ct_assert_equal(output "libAlibB")
+	endfunction()
+	
+	ct_add_section(NAME "strip_nothing_no_interfaces_detected")
+	function(${CMAKETEST_SECTION})
+		set(input "libA;libB")
+		string_manip(STRIP_INTERFACES input)
+		ct_assert_list(input)
+		ct_assert_equal(input "libA;libB")
+
+		set(input "libAlibB")
+		string_manip(STRIP_INTERFACES input)
+		ct_assert_string(input)
+		ct_assert_equal(input "libAlibB")
+	endfunction()
+
+	ct_add_section(NAME "strip_nothing_no_interfaces_detected_output_version")
+	function(${CMAKETEST_SECTION})
+		set(input "libA;libB")
+		unset(output)
+		string_manip(STRIP_INTERFACES input OUTPUT_VARIABLE output)
+		ct_assert_equal(input "libA;libB")
+		ct_assert_list(output)
+		ct_assert_equal(output "libA;libB")
+
+		set(input "libAlibB")
+		unset(output)
+		string_manip(STRIP_INTERFACES input OUTPUT_VARIABLE output)
+		ct_assert_equal(input "libAlibB")
+		ct_assert_string(output)
+		ct_assert_equal(output "libAlibB")
+	endfunction()
+
+	ct_add_section(NAME "strip_all_when_only_interfaces_detected")
+	function(${CMAKETEST_SECTION})
+		set(input "$<BUILD_INTERFACE:src>;$<INSTALL_INTERFACE:include/>")
+		string_manip(STRIP_INTERFACES input)
+		ct_assert_string(input)
+		ct_assert_equal(input "")
+
+		set(input "$<BUILD_INTERFACE:src>$<INSTALL_INTERFACE:include/>")
+		string_manip(STRIP_INTERFACES input)
+		ct_assert_string(input)
+		ct_assert_equal(input "")
+	endfunction()
+
+	ct_add_section(NAME "strip_all_when_only_interfaces_detected_output_version")
+	function(${CMAKETEST_SECTION})
+		set(input "$<BUILD_INTERFACE:src>;$<INSTALL_INTERFACE:include/>")
+		unset(output)
+		string_manip(STRIP_INTERFACES input OUTPUT_VARIABLE output)
+		ct_assert_equal(input "$<BUILD_INTERFACE:src>;$<INSTALL_INTERFACE:include/>")
+		ct_assert_string(output)
+		ct_assert_equal(output "")
+
+		set(input "$<BUILD_INTERFACE:src>$<INSTALL_INTERFACE:include/>")
+		unset(output)
+		string_manip(STRIP_INTERFACES input OUTPUT_VARIABLE output)
+		ct_assert_equal(input "$<BUILD_INTERFACE:src>$<INSTALL_INTERFACE:include/>")
+		ct_assert_string(output)
+		ct_assert_equal(output "")
+	endfunction()
+	
+	# Errors checking
+	ct_add_section(NAME "throws_if_arg_string_var_is_missing_1" EXPECTFAIL)
+	function(${CMAKETEST_SECTION})
+		string_manip(STRIP_INTERFACES)
+	endfunction()
+
+	ct_add_section(NAME "throws_if_arg_string_var_is_missing_2" EXPECTFAIL)
+	function(${CMAKETEST_SECTION})
+		string_manip(STRIP_INTERFACES "")
+	endfunction()
+
+	ct_add_section(NAME "throws_if_arg_string_var_is_missing_3" EXPECTFAIL)
+	function(${CMAKETEST_SECTION})
+		string_manip(STRIP_INTERFACES "foo")
+	endfunction()
+
+	ct_add_section(NAME "throws_if_arg_string_var_is_missing_4" EXPECTFAIL)
+	function(${CMAKETEST_SECTION})
+		unset(input)
+		string_manip(STRIP_INTERFACES input)
+	endfunction()
+
+	ct_add_section(NAME "throws_if_arg_output_var_is_missing_1" EXPECTFAIL)
+	function(${CMAKETEST_SECTION})
+		set(input "mystring")
+		string_manip(STRIP_INTERFACES input OUTPUT_VARIABLE)
+	endfunction()
+
+	ct_add_section(NAME "throws_if_arg_output_var_is_missing_2" EXPECTFAIL)
+	function(${CMAKETEST_SECTION})
+		set(input "mystring")
+		string_manip(STRIP_INTERFACES input OUTPUT_VARIABLE "")
+	endfunction()
+
+	ct_add_section(NAME "throws_if_arg_output_var_is_missing_3" EXPECTFAIL)
+	function(${CMAKETEST_SECTION})
+		set(input "mystring")
+		string_manip(STRIP_INTERFACES input OUTPUT_VARIABLE "foo")
+	endfunction()
+
+	ct_add_section(NAME "throws_if_arg_output_var_is_missing_4" EXPECTFAIL)
+	function(${CMAKETEST_SECTION})
+		set(input "mystring")
+		unset(output)
+		string_manip(STRIP_INTERFACES input OUTPUT_VARIABLE output)
+	endfunction()
+endfunction()
