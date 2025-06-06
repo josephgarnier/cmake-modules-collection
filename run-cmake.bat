488a32cb8bb5ee145c6cd4bbdfb70bbec0a84212
Joseph Garnier <garnjose@gmail.com>
2025-06-06 23:07:59 +0200

    Update command

--- run-cmake.bat
+++ run-cmake.bat
@@ -16,7 +16,7 @@ set "WORKSPACE_DIR=%cd%"
 cmake --log-level=TRACE -S "%WORKSPACE_DIR%" --preset "x64-Release-Win-GCC"
 
 rem To debug your CMakeLists.txt, use the command below and comment the previous one
-rem cmake --debug-output --trace-expand --log-level=TRACE -S "%WORKSPACE_DIR%" --preset "x64-Release-Win-GCC"
+rem cmake --debug-output --trace-expand --log-level=TRACE -S "%WORKSPACE_DIR%" --preset "x64-Release-Win-GCC" --trace-redirect=output.log
 
 if %errorlevel%==0 (
 	echo.The solution was successfully generated!
