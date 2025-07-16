80b45546560be3e66840fe90d17d5306965e182e
Joseph Garnier <garnjose@gmail.com>
2025-07-16 12:26:06 +0200

    Fix bad prop initialization

--- tests/dependency/dependency_ADD_INCLUDE_DIRECTORIES.cmake
+++ tests/dependency/dependency_ADD_INCLUDE_DIRECTORIES.cmake
@@ -92,16 +92,13 @@ function(${CMAKETEST_TEST})
 	# To call before each test
 	macro(_set_up_test)
 		# Reset properties set by `dependency(ADD_INCLUDE_DIRECTORIES)`
-		set_target_properties("imp_static_mock_lib" PROPERTIES
-			INTERFACE_INCLUDE_DIRECTORIES ""
-			INTERFACE_INCLUDE_DIRECTORIES_BUILD ""
-			INTERFACE_INCLUDE_DIRECTORIES_INSTALL ""
-		)
-		set_target_properties("imp_shared_mock_lib" PROPERTIES
-			INTERFACE_INCLUDE_DIRECTORIES ""
-			INTERFACE_INCLUDE_DIRECTORIES_BUILD ""
-			INTERFACE_INCLUDE_DIRECTORIES_INSTALL ""
-		)
+		set_property(TARGET "imp_static_mock_lib" PROPERTY INTERFACE_INCLUDE_DIRECTORIES)
+		set_property(TARGET "imp_static_mock_lib" PROPERTY INTERFACE_INCLUDE_DIRECTORIES_BUILD)
+		set_property(TARGET "imp_static_mock_lib" PROPERTY INTERFACE_INCLUDE_DIRECTORIES_INSTALL)
+
+		set_property(TARGET "imp_shared_mock_lib" PROPERTY INTERFACE_INCLUDE_DIRECTORIES)
+		set_property(TARGET "imp_shared_mock_lib" PROPERTY INTERFACE_INCLUDE_DIRECTORIES_BUILD)
+		set_property(TARGET "imp_shared_mock_lib" PROPERTY INTERFACE_INCLUDE_DIRECTORIES_INSTALL)
 	endmacro()
 
 	# Functionalities checking
