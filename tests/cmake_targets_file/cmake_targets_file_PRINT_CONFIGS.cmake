71e3413ef1805f00e82e58f15fbb61d97b649ea4
Joseph Garnier <garnjose@gmail.com>
2025-08-29 13:32:48 +0200

    Fix misnamed var

--- tests/cmake_targets_file/cmake_targets_file_PRINT_CONFIGS.cmake
+++ tests/cmake_targets_file/cmake_targets_file_PRINT_CONFIGS.cmake
@@ -43,7 +43,6 @@ function(${CMAKETEST_TEST})
     "name:apple"
     "type:staticLib"
     "mainFile:src/apple/main.cpp"
-    "pchFile:src/apple/apple_pch.h"
     "build.compileFeatures:"
     "build.compileDefinitions:"
     "build.compileOptions:"
@@ -57,7 +56,6 @@ function(${CMAKETEST_TEST})
     "name:banana"
     "type:staticLib"
     "mainFile:src/banana/main.cpp"
-    "pchFile:src/banana/banana_pch.h"
     "build.compileFeatures:"
     "build.compileDefinitions:"
     "build.compileOptions:"
@@ -84,8 +82,8 @@ function(${CMAKETEST_TEST})
     _set_up_test()
     set_property(GLOBAL PROPERTY TARGETS_CONFIG_LOADED "on")
     set_property(GLOBAL PROPERTY TARGETS_CONFIG_LIST "src;src/apple;src/banana")
-    set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src" "${input_src_apple_config}")
-    set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/apple" "${input_src_config}")
+    set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src" "${input_src_config}")
+    set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/apple" "${input_src_apple_config}")
     set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/banana" "${input_src_banana_config}")
     cmake_targets_file(PRINT_CONFIGS)
   endfunction()
@@ -104,8 +102,8 @@ function(${CMAKETEST_TEST})
       ct_assert_equal(output_property "")
       ct_assert_not_defined(output_property)
       set_property(GLOBAL PROPERTY TARGETS_CONFIG_LIST "src;src/apple;src/banana")
-      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src" "${input_src_apple_config}")
-      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/apple" "${input_src_config}")
+      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src" "${input_src_config}")
+      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/apple" "${input_src_apple_config}")
       set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/banana" "${input_src_banana_config}")
 
       ct_add_section(NAME "throws_if_global_property_not_set_1_inner" EXPECTFAIL)
@@ -124,8 +122,8 @@ function(${CMAKETEST_TEST})
       get_property(output_property GLOBAL PROPERTY "TARGETS_CONFIG_LIST")
       ct_assert_equal(output_property "")
       ct_assert_not_defined(output_property)
-      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src" "${input_src_apple_config}")
-      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/apple" "${input_src_config}")
+      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src" "${input_src_config}")
+      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/apple" "${input_src_apple_config}")
       set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/banana" "${input_src_banana_config}")
 
       ct_add_section(NAME "throws_if_global_property_not_set_2_inner" EXPECTFAIL)
@@ -145,7 +143,7 @@ function(${CMAKETEST_TEST})
       get_property(output_property GLOBAL PROPERTY "TARGETS_CONFIG_src")
       ct_assert_equal(output_property "")
       ct_assert_not_defined(output_property)
-      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/apple" "${input_src_config}")
+      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/apple" "${input_src_apple_config}")
       set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/banana" "${input_src_banana_config}")
 
       ct_add_section(NAME "throws_if_global_property_not_set_3_inner" EXPECTFAIL)
@@ -159,8 +157,8 @@ function(${CMAKETEST_TEST})
       _set_up_test()
       set_property(GLOBAL PROPERTY TARGETS_CONFIG_LOADED "")
       set_property(GLOBAL PROPERTY TARGETS_CONFIG_LIST "src;src/apple;src/banana")
-      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src" "${input_src_apple_config}")
-      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/apple" "${input_src_config}")
+      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src" "${input_src_config}")
+      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/apple" "${input_src_apple_config}")
       set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/banana" "${input_src_banana_config}")
     
       ct_add_section(NAME "throws_if_global_property_is_empty_1_inner" EXPECTFAIL)
@@ -174,8 +172,8 @@ function(${CMAKETEST_TEST})
       _set_up_test()
       set_property(GLOBAL PROPERTY TARGETS_CONFIG_LOADED "on")
       set_property(GLOBAL PROPERTY TARGETS_CONFIG_LIST "")
-      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src" "${input_src_apple_config}")
-      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/apple" "${input_src_config}")
+      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src" "${input_src_config}")
+      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/apple" "${input_src_apple_config}")
       set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/banana" "${input_src_banana_config}")
     
       ct_add_section(NAME "throws_if_global_property_is_empty_2_inner" EXPECTFAIL)
@@ -190,7 +188,7 @@ function(${CMAKETEST_TEST})
       set_property(GLOBAL PROPERTY TARGETS_CONFIG_LOADED "on")
       set_property(GLOBAL PROPERTY TARGETS_CONFIG_LIST "src;src/apple;src/banana")
       set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src" "")
-      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/apple" "${input_src_config}")
+      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/apple" "${input_src_apple_config}")
       set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/banana" "${input_src_banana_config}")
     
       ct_add_section(NAME "throws_if_global_property_is_empty_3_inner" EXPECTFAIL)
@@ -204,8 +202,8 @@ function(${CMAKETEST_TEST})
       _set_up_test()
       set_property(GLOBAL PROPERTY TARGETS_CONFIG_LOADED "not-bool")
       set_property(GLOBAL PROPERTY TARGETS_CONFIG_LIST "src;src/apple;src/banana")
-      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src" "${input_src_apple_config}")
-      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/apple" "${input_src_config}")
+      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src" "${input_src_config}")
+      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/apple" "${input_src_apple_config}")
       set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/banana" "${input_src_banana_config}")
     
       ct_add_section(NAME "throws_if_global_property_is_not_bool_inner" EXPECTFAIL)
@@ -219,8 +217,8 @@ function(${CMAKETEST_TEST})
       _set_up_test()
       set_property(GLOBAL PROPERTY TARGETS_CONFIG_LOADED "off")
       set_property(GLOBAL PROPERTY TARGETS_CONFIG_LIST "src;src/apple;src/banana")
-      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src" "${input_src_apple_config}")
-      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/apple" "${input_src_config}")
+      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src" "${input_src_config}")
+      set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/apple" "${input_src_apple_config}")
       set_property(GLOBAL PROPERTY "TARGETS_CONFIG_src/banana" "${input_src_banana_config}")
 
       ct_add_section(NAME "throws_if_global_property_is_off_inner" EXPECTFAIL)
@@ -228,53 +226,5 @@ function(${CMAKETEST_TEST})
         cmake_targets_file(PRINT_CONFIGS)
       endfunction()
     endfunction()
-  endfunction()
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  ct_add_section(NAME "throws_if_arg_x_is_missing_1" EXPECTFAIL)
-  function(${CMAKETEST_SECTION})
-    func(OP)
-  endfunction()
-
-  ct_add_section(NAME "throws_if_arg_x_is_missing_2" EXPECTFAIL)
-  function(${CMAKETEST_SECTION})
-    func(OP "")
-  endfunction()
-
-  ct_add_section(NAME "throws_if_arg_x_var_is_missing_1" EXPECTFAIL)
-  function(${CMAKETEST_SECTION})
-    func(OP)
-  endfunction()
-
-  ct_add_section(NAME "throws_if_arg_x_var_is_missing_2" EXPECTFAIL)
-  function(${CMAKETEST_SECTION})
-    func(OP "")
-  endfunction()
-  
-  ct_add_section(NAME "throws_if_arg_x_var_is_missing_3" EXPECTFAIL)
-  function(${CMAKETEST_SECTION})
-    func(OP "foo")
-  endfunction()
-  
-  ct_add_section(NAME "throws_if_arg_output_var_is_missing_1" EXPECTFAIL)
-  function(${CMAKETEST_SECTION})
-    func(OP "foo" OUTPUT_VARIABLE)
-  endfunction()
-
-  ct_add_section(NAME "throws_if_arg_output_var_is_missing_2" EXPECTFAIL)
-  function(${CMAKETEST_SECTION})
-    func(OP foo OUTPUT_VARIABLE "")
-  endfunction()
-
-  ct_add_section(NAME "throws_if_arg_output_var_is_missing_3" EXPECTFAIL)
-  function(${CMAKETEST_SECTION})
-    func(OP foo OUTPUT_VARIABLE "foo")
   endfunction()
 endfunction()
